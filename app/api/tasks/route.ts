ca2b7296b6128b446962ac889285972f667b71e2
Oscar Espana <espaaoscar@gmail.com>
2023-10-17 23:02:32 -0500

    feat: :recycle: fix the return data in tasks api

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -3,7 +3,8 @@ import construexApi from "@/api/construexApi";
 export async function GET() {
   try {
     const response = await construexApi.get("/get-tasks-success.json");
-    return Response.json({ data: response.data }, { status: response.status });
+    const data = response.data;
+    return Response.json(data, { status: response.status });
   } catch (error) {
     return Response.json({ error: error }, { status: 500 });
   }
